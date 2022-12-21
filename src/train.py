1889a1098b029532dec1dad058652bc454fef0a2
Simone Persiani <iosonopersia@gmail.com>
2022-12-21 19:10:14 +0100

    Add gradients clipping to training loop

--- src/train.py
+++ src/train.py
@@ -231,6 +231,7 @@ def train(model, feature_embedding_model, dl_train, criterion, optimizer, epoch,
         outputs = model(text, audio, mask)
         loss = criterion(outputs.permute(0, 2, 1), emotion)
         loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
         optimizer.step()
 
         loss_train += loss.item()
