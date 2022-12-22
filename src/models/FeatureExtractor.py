Fix attention mask by inverting it

src/models/FeatureExtractor.py
@@ -69,9 +69,9 @@ class FeatureExtractor(torch.nn.Module):
         batch_audio_features, _ = apply_padding(batch_audio_features)
 
         # Lengths to attention masks:
-        attention_mask = torch.zeros(batch_text_features.shape[0], batch_text_features.shape[1], dtype=torch.bool)
+        attention_mask = torch.ones(batch_text_features.shape[0], batch_text_features.shape[1],
+                                    dtype=torch.bool, device=batch_text_features.device)
         for i in range(batch_text_features.shape[0]):
-            attention_mask[i, :dialogue_lengths[i]] = True
-        attention_mask = attention_mask.to(batch_text_features.device)
+            attention_mask[i, :dialogue_lengths[i]] = False
 
         return batch_text_features, batch_audio_features, attention_mask

src/models/M2FNet.py
@@ -85,9 +85,9 @@ class M2FNet(nn.Module):
         # (batch_size, seq_len, seq_len)
         squared_mask = mask.unsqueeze(1).repeat(1, mask.shape[1], 1)
         squared_mask = squared_mask & squared_mask.transpose(1, 2)
-        squared_mask_text = squared_mask.repeat(self.n_head_text, 1, 1).float()
-        squared_mask_audio = squared_mask.repeat(self.n_head_audio, 1, 1).float()
-        squared_mask_fam = squared_mask.repeat(self.n_head_fam, 1, 1).float()
+        squared_mask_text = squared_mask.repeat(self.n_head_text, 1, 1)
+        squared_mask_audio = squared_mask.repeat(self.n_head_audio, 1, 1)
+        squared_mask_fam = squared_mask.repeat(self.n_head_fam, 1, 1)
 
         # Add skip connections to audio encoders
         for encoder in self.audio_encoders: