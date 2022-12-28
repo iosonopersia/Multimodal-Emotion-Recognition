448ad9a10a749d25f63cbf41b9823de5b3727dd0
Simone Persiani <iosonopersia@gmail.com>
2022-12-28 13:57:19 +0100

    Fix encoding of MELD utterances

--- src/utils/dataset_utils.py
+++ src/utils/dataset_utils.py
@@ -17,8 +17,20 @@ def get_text (mode="train"):
     if not os.path.exists(data_path):
         raise ValueError("Dataset not found at {}".format(data_path))
 
-    df = pd.read_csv(data_path, usecols=['Utterance', 'Emotion', 'Dialogue_ID', 'Utterance_ID'], encoding="utf-8")
-    df["Utterance"] = df["Utterance"].map(lambda x: x.replace("\u0092", "'"))
+    df = pd.read_csv(data_path, usecols=['Utterance', 'Emotion', 'Dialogue_ID', 'Utterance_ID'])
+
+    cp1252_to_utf8 = {
+        "\x85": "…", # \x85 \u2026 HORIZONTAL ELLIPSIS
+        "\x91": "‘", # \x91 \u2018 LEFT SINGLE QUOTATION MARK
+        "\x92": "’", # \x92 \u2019 RIGHT SINGLE QUOTATION MARK
+        "\x93": "“", # \x93 \u201C LEFT DOUBLE QUOTATION MARK
+        "\x94": "”", # \x94 \u201D RIGHT DOUBLE QUOTATION MARK
+        "\x96": "–", # \x96 \u2013 EN DASH
+        "\x97": "—", # \x97 \u2014 EM DASH
+        "\xa0": " ", # \xa0 \u00A0 NO-BREAK SPACE
+    }
+    for key, value in cp1252_to_utf8.items():
+        df["Utterance"] = df["Utterance"].map(lambda x: x.replace(key, value))
 
     return df
 
