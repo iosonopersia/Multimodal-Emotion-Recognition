466fea509953b43e9ed2934ed3f1f8bdda1f73d3
LudovicoGranata <ludogranata@gmail.com>
2022-12-30 00:51:50 +0100

    Implementation of VarianceLoss

---
+++ src/losses/VarianceLoss.py
@@ -0,0 +1,19 @@
+import torch
+
+class VarianceLoss(torch.nn.Module):
+    def __init__(self, eps=1e-6):
+        super(VarianceLoss, self).__init__()
+        self.eps = eps
+
+    def forward(self, za, zp, zn):
+        var_loss = 0
+        for z in [za, zp, zn]:
+            var_loss += self.Lvar(z)
+        return var_loss
+
+    def Lvar(self, z):
+
+        var_z = torch.var(z, dim=0)
+        result = (1 - torch.sqrt(var_z + self.eps)).mean()
+        return result
+
