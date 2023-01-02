2373c68b87e2161b0fdf63caab8aad6ff2850fc8
LudovicoGranata <ludogranata@gmail.com>
2023-01-02 15:21:12 +0100

    add model normalization

--- src/feature_extractors/audio_mel/AudioMelFeatureExtractor.py
+++ src/feature_extractors/audio_mel/AudioMelFeatureExtractor.py
@@ -2,6 +2,11 @@ import torch
 import torch.nn as nn
 from torchvision.models import resnet18
 
+from losses.AdaptiveTripletMarginLoss import AdaptiveTripletMarginLoss
+from losses.CovarianceLoss import CovarianceLoss
+from losses.VarianceLoss import VarianceLoss
+from torch.nn.functional import normalize
+
 '''here we use ResNet18 as the backbone of the encoder network while the projector con-
 sists a linear fully connected layer which project the em-
 bedding of encoder network to desired representations'''
@@ -10,15 +15,18 @@ class AudioMelFeatureExtractor(nn.Module):
     def __init__(self):
         super(AudioMelFeatureExtractor, self).__init__()
         self.resnet18 = resnet18(weights=None)
-        # self.resnet18.fc = nn.Identity()
+        self.resnet18.fc = nn.Identity()
         self.projector = nn.Sequential(
-            nn.Linear(1000, 768),
             nn.ReLU(),
-            nn.Linear(768, 300)
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, 300)
         )
     def forward(self, x):
         x = self.resnet18(x)
         x = self.projector(x)
+        x = normalize(x, p=2, dim=1)
+
         return x
 
 # add to AudioMelFeatureExtractor class a classification head for emotion (output 7 classes)
