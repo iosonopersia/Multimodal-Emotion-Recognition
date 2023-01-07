ce2203ccb48447f48b452f689d72d6a3f1b36d34
Simone Persiani <iosonopersia@gmail.com>
2023-01-07 23:11:05 +0100

    Remove useless arguments when executing RoBERTa

--- src/feature_extractors/text/model.py
+++ src/feature_extractors/text/model.py
@@ -17,12 +17,7 @@ class TextERC(torch.nn.Module):
         self.classifier_head = RobertaClassificationHead(config)
 
     def forward(self, input_ids, attention_mask):
-        text_features = self.roberta(
-            input_ids=input_ids,
-            attention_mask=attention_mask,
-            # token_type_ids=None, # TODO
-            # position_ids=None, # TODO
-        )
+        text_features = self.roberta(input_ids=input_ids, attention_mask=attention_mask)
         text_features = text_features.last_hidden_state
         out = self.classifier_head(text_features)
         return out
