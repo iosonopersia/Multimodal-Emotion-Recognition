29e9b4e66eb620b903cc05cbed29574e4fb2ffc6
Simone Persiani <iosonopersia@gmail.com>
2023-01-08 20:42:52 +0100

    Cleanup VarianceLoss.py

--- src/feature_extractors/audio_mel/losses/VarianceLoss.py
+++ src/feature_extractors/audio_mel/losses/VarianceLoss.py
@@ -1,20 +1,25 @@
 import torch
 import torch.nn.functional as F
 
+
+# Source: https://arxiv.org/pdf/2105.04906.pdf
+def variance_regularization_term(z, gamma=1.0, eps=1e-6):
+    # z is a tensor of shape (batch_size, num_features)
+    var_z = torch.var(z, dim=0)
+
+    d = z.shape[-1]
+    var_z = F.relu(gamma - torch.sqrt(var_z + eps))
+    return torch.sum(var_z) / d
+
+
 class VarianceLoss(torch.nn.Module):
-    def __init__(self, eps=1e-6):
+    def __init__(self, gamma=1.0, eps=1e-6):
         super(VarianceLoss, self).__init__()
+        self.gamma = gamma
         self.eps = eps
 
     def forward(self, za, zp, zn):
-        var_loss = 0
+        var_loss = 0.0
         for z in [za, zp, zn]:
-            var_loss += self.Lvar(z)
-        return max(0,var_loss)
-
-    def Lvar(self, z):
-
-        var_z = torch.var(z, dim=0)
-        result = F.relu((1 - torch.sqrt(var_z + self.eps))).mean()
-        return result
-
+            var_loss += variance_regularization_term(z, gamma=self.gamma, eps=self.eps)
+        return var_loss
