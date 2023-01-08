f021f1647995bcc375432ea57e774356e1e6b436
Simone Persiani <iosonopersia@gmail.com>
2023-01-08 20:30:54 +0100

    Cleanup CovarianceLoss.py

--- src/feature_extractors/audio_mel/losses/CovarianceLoss.py
+++ src/feature_extractors/audio_mel/losses/CovarianceLoss.py
@@ -1,23 +1,23 @@
 import torch
 
+
+# Source: https://arxiv.org/pdf/2105.04906.pdf
+def covariance_regularization_term(z):
+    # z is a tensor of shape (batch_size, num_features)
+    cov_z = torch.cov(z.T)
+    cov_z = torch.pow(cov_z, 2)
+
+    d = z.shape[-1]
+    cov_z.diagonal()[:] = 0.0
+    return cov_z.sum() / d
+
+
 class CovarianceLoss(torch.nn.Module):
-    def __init__(self, eps=1e-6):
+    def __init__(self):
         super(CovarianceLoss, self).__init__()
-        self.eps = eps
 
     def forward(self, za, zp, zn):
-        cov_loss = 0
+        cov_loss = 0.0
         for z in [za, zp, zn]:
-            cov_loss += self.Lcov(z)
-        return max(0,cov_loss)
-
-    def Lcov(self, z):
-        cov_z = torch.cov(z.T)
-        # put the diagonal to zero
-        cov_z = cov_z - torch.diag(torch.diag(cov_z))
-        cov_z = torch.pow(cov_z, 2)
-        return cov_z.sum()/ cov_z.shape[-1]
-
-
-
-
+            cov_loss += covariance_regularization_term(z)
+        return cov_loss
